4c6ca2c6b2a9e7f3537cc2b79acb05f1dcae86b4
Ashlin Aronin & Adam Won & Austin Blanchard <student@epicodus.com>
2015-09-10 12:07:27 -0700

    Write combined-case specs and make them fail

--- spec/specs.js
+++ spec/specs.js
@@ -18,7 +18,7 @@ describe('numeralGenerator', function() {
     it ("returns C for an input of 100", function(){
         expect(numeralGenerator(100)).to.equal("C");
     });
-    
+
     it ("returns D for an input of 500", function(){
         expect(numeralGenerator(500)).to.equal("D");
     });
@@ -26,4 +26,38 @@ describe('numeralGenerator', function() {
     it ("returns M for an input of 1000", function(){
         expect(numeralGenerator(1000)).to.equal("M");
     });
+
+    it ("returns II for an input of 2", function(){
+        expect(numeralGenerator(2)).to.equal("II");
+    });
+
+    it ("returns III for an input of 3", function(){
+        expect(numeralGenerator(3)).to.equal("III");
+    });
+
+    it ("returns IV for an input of 4", function(){
+        expect(numeralGenerator(4)).to.equal("IV");
+    });
+
+    it ("returns VI for an input of 6", function(){
+        expect(numeralGenerator(6)).to.equal("VI");
+    });
+
+    it ("returns IX for an input of 9", function(){
+        expect(numeralGenerator(9)).to.equal("IX");
+    });
+
+    it ("returns XIV for an input of 14", function(){
+        expect(numeralGenerator(14)).to.equal("XIV");
+    });
+    it ("returns LXVI for an input of 66", function(){
+        expect(numeralGenerator(66)).to.equal("LXVI");
+    });
+    it ("returns XC for an input of 90", function(){
+        expect(numeralGenerator(90)).to.equal("XC");
+    });
+    it ("returns XCIX for an input of 99", function(){
+        expect(numeralGenerator(99)).to.equal("XCIX");
+    });
+
 });
