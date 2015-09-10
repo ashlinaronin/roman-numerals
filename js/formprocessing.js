736c22a8e8cbbdb2c4541cc15d39f3fbaafa82dc
Ashlin Aronin & Adam Won & Austin Blanchard <student@epicodus.com>
2015-09-10 16:05:05 -0700

    create front-end for Roman Numeral

--- js/formprocessing.js
+++ js/formprocessing.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
-    $('form#count-up-by').submit(function(event) {
-        var multiple = parseInt($('input#multiple').val());
-        var goal = parseInt($('input#goal').val());
-
-        var resultArray = countUpBy(multiple, goal);
+    $('form#roman-number').submit(function(event) {
+        var inputtedNumber = parseInt($('input#number').val());
+        var resultString = numeralGenerator(inputtedNumber);
 
         // Clear results before adding new ones
         $('#result').empty();
 
-        // Print each result as a list item
-        resultArray.forEach(function(result) {
-            $('#result').append('<li>' + result + '</li>');
-        });
+        // Add the result string into the result div
+        $('#result').text(resultString);
 
         // Show the hidden #result div
         $('#result').show();
 
+        // Avoid automatic browser form submit refresh confusion
         event.preventDefault();
     });
 });
