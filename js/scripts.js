a148489e77cf4cf103cca733f98e745620379a08
Ashlin Aronin & Adam Won & Austin Blanchard <student@epicodus.com>
2015-09-10 14:11:22 -0700

    Add functionality for X

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,37 @@ var numeralGenerator = function(number){
         1000: 'M'
     };
 
+    var result = "";
 
-    return numerals[number];
+    // Handle numbers under eight inclusive for now
+
+    // Number of Xs is integer division by 10
+    var numberXs = Math.trunc(number / 10);
+    var xRemainder = (number % 10);
+
+    // Number of Vs is integer division by 5
+    var numberVs = Math.trunc(xRemainder / 5);
+
+    // Number of Is is remainder on division by 5
+    var numberIs = (number % 5);
+
+
+    // Add Xs to result string
+    for (var i = 0; i < numberXs; i++) {
+        result += "X";
+    }
+
+    // Add Vs to result string
+    for (var i = 0; i < numberVs; i++) {
+        result += "V";
+    }
+
+    // Add Is to result string
+    for (var i = 0; i < numberIs; i++) {
+        result += "I";
+    }
+
+    return result;
 
     // if (number === 1){
     //     return 'I';
